2f9604ad8c90b657fc17bc4e9154f2f0d1bda5b1
Robert Shum <robertshum@gmail.com>
2023-10-02 21:47:51 -0700

    added new view for creating new URLs, added pseudo code for gen. random string.

--- express_server.js
+++ express_server.js
@@ -5,10 +5,27 @@ const PORT = 8080; // default port 8080
 //Use Embeded JS
 app.set("view engine", "ejs");
 
+//Used so we can populate body for POST requests
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//TODO move somewhere else buckaroo later
+function generateRandomString() {
+  //do this 6 times
+    //flip a coin (for upper or lower)
+      //heads - lower case
+        //pick a range from 77-122, randomly
+        //convert that to a character. String.fromCharCode()
+        //push that to an array.
+      //tails - upper case
+        //pick a range randomly from 65-90, randomly
+        //do same thing as above
+  //return array
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -17,6 +34,10 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -30,6 +51,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
