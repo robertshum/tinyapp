b9dd661663e76ee00a35df288a05e0fb52b36b26
Robert Shum <robertshum@gmail.com>
2023-10-02 20:57:46 -0700

    added individual URL routes.

--- express_server.js
+++ express_server.js
@@ -16,7 +16,12 @@ app.get("/", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars)
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
