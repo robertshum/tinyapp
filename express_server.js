e193db6a13fc3f5790245e6327c157a88336f088
Robert Shum <robertshum@gmail.com>
2023-10-03 14:24:59 -0700

    exposed userName var to partial headers and templates in endpoints.  Added logic to display appropriate username or login text from cookies in header.

--- express_server.js
+++ express_server.js
@@ -1,4 +1,7 @@
+//express for endpoints
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -7,8 +10,10 @@ const PORT = 8080; // default port 8080
 //Use Embeded JS
 app.set("view engine", "ejs");
 
-//Used so we can populate body for POST requests
+//Middleware - Used so we can populate body for POST requests
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -47,21 +52,47 @@ function generateRandomString() {
   return results;
 }
 
+function getCookies(req, input) {
+  if (input === "userName") {
+    const name = req.cookies[input];
+
+    //no userName set
+    if (name === undefined || name === "") {
+      return "Login:";
+    }
+
+    //there is a username
+    return `Welcome back, ${name}`;
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+
+  const templateVars = {
+    urls: urlDatabase,
+    userName: getCookies(req, "userName")
+  };
+  console.log(templateVars);
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    userName: getCookies(req, "userName")
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    userName: getCookies(req, "userName")
+  };
   res.render("urls_show", templateVars);
 });
 
